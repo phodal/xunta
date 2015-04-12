7964da6e5df57665e3ebd5d851c2b4b4cae058d4
Fengda HUANG <h@phodal.com>
2015-04-12 23:58:11 +0800

    fix templates

--- urls.py
+++ urls.py
@@ -1,16 +1,18 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from mezzanine.core.views import direct_to_template
 
 
 admin.autodiscover()
 
 
 urlpatterns = patterns("",
+    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
     ("^admin/", include(admin.site.urls)),
+    ("^", include("mezzanine.urls")),
     ("^", include("drum.links.urls")),
     ("^", include("juba.urls")),
-    ("^", include("mezzanine.urls")),
 )
 
 # Adds ``STATIC_URL`` to the context.

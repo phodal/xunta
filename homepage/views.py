e89c63b31b7630a7c0e0c4d192ab31e62e1cfcab
Fengda HUANG <h@phodal.com>
2015-04-13 12:59:50 +0800

    add all object in homepage

--- homepage/views.py
+++ homepage/views.py
@@ -16,6 +16,7 @@ from mezzanine.utils.views import paginate
 
 from drum.links.models import Link
 from drum.links.utils import order_by_score
+from juba.models import Juba
 
 
 User = get_user_model()
@@ -60,43 +61,52 @@ class LinkView(object):
         return Link.objects.published().select_related("user", "user__profile")
 
 
-def get_blog_posts(category, month, request, tag, template, username, year):
+class HomepageList(LinkView, ScoreOrderingView):
+    """
+    List view for links, which can be for all users (homepage) or
+    a single user (links from user's profile page). Links can be
+    order by score (homepage, profile links) or by most recently
+    created ("newest" main nav item).
+    """
+
+    date_field = "publish_date"
+    score_fields = ["rating_sum", "comments_count"]
+
+    def get_queryset(self):
+        queryset = super(HomepageList, self).get_queryset()
+        tag = self.kwargs.get("tag")
+        if tag:
+            queryset = queryset.filter(keywords__keyword__slug=tag)
+        return queryset.prefetch_related("keywords__keyword")
+
+    def get_title(self, context):
+        tag = self.kwargs.get("tag")
+        if tag:
+            return get_object_or_404(Keyword, slug=tag).title
+        if context["by_score"]:
+            return ""  # Homepage
+        if context["profile_user"]:
+            return "Links by %s" % context["profile_user"].profile
+        else:
+            return "Newest"
+
+
+def homepage(request, tag=None, year=None, month=None, username=None,
+             category=None, template="index.html"):
+    settings.use_editable()
     templates = []
     blog_posts = BlogPost.objects.published(for_user=request.user)
-    if tag is not None:
-        tag = get_object_or_404(Keyword, slug=tag)
-        blog_posts = blog_posts.filter(keywords__keyword=tag)
-    if year is not None:
-        blog_posts = blog_posts.filter(publish_date__year=year)
-        if month is not None:
-            blog_posts = blog_posts.filter(publish_date__month=month)
-            try:
-                month = month_name[int(month)]
-            except IndexError:
-                raise Http404()
-    if category is not None:
-        category = get_object_or_404(BlogCategory, slug=category)
-        blog_posts = blog_posts.filter(categories=category)
-        templates.append(u"blog/blog_post_list_%s.html" %
-                         str(category.slug))
     author = None
-    if username is not None:
-        author = get_object_or_404(User, username=username)
-        blog_posts = blog_posts.filter(user=author)
-        templates.append(u"blog/blog_post_list_%s.html" % username)
+
     prefetch = ("categories", "keywords__keyword")
     blog_posts = blog_posts.select_related("user").prefetch_related(*prefetch)
+    links = Link.objects.published().select_related("user", "user__profile").prefetch_related()
+    jubas = Juba.objects.published().select_related("user", "user__profile").prefetch_related()
+
     blog_posts = paginate(blog_posts, request.GET.get("page", 1),
                           settings.BLOG_POST_PER_PAGE,
                           settings.MAX_PAGING_LINKS)
     context = {"blog_posts": blog_posts, "year": year, "month": month,
-               "tag": tag, "category": category, "author": author}
+               "tag": tag, "category": category, "author": author, "links": links, "jubas": jubas}
     templates.append(template)
-    return context, templates
-
-
-def homepage(request, tag=None, year=None, month=None, username=None,
-             category=None, template="blog/blog_post_list.html"):
-    settings.use_editable()
-    context, templates = get_blog_posts(category, month, request, tag, template, username, year)
     return render(request, templates, context)

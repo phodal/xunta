7599a7fb11b7c723d56582721e23559c5bcf73dc
Fengda HUANG <h@phodal.com>
2015-04-14 21:04:32 +0800

    Refactor api

---
+++ api/blog_api.py
@@ -0,0 +1,26 @@
+from mezzanine.blog.models import BlogPost
+from rest_framework import serializers, viewsets
+from rest_framework import filters
+
+class BlogpostListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'slug')
+
+
+class BlogPostListSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogpostListSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('title', 'slug')
+
+
+class BlogpostDetailSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'slug', 'description', 'content')
+
+
+class BlogPostDetailSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogpostDetailSerializer
--- api/urls.py
+++ api/urls.py
@@ -1,40 +1,14 @@
 from django.conf.urls import url, include
-from mezzanine.blog.models import BlogPost
-from rest_framework import serializers, viewsets, routers
-from rest_framework import filters
+from rest_framework import routers
+
+from api.blog_api import BlogPostListSet, BlogPostDetailSet
 
 
-class BlogpostListSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = BlogPost
-        fields = ('title', 'slug')
-
-
-class BlogPostListSet(viewsets.ModelViewSet):
-    queryset = BlogPost.objects.all()
-    serializer_class = BlogpostListSerializer
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('title', 'slug')
-
-
-class BlogpostDetailSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = BlogPost
-        fields = ('title', 'slug', 'description', 'content')
-
-
-class BlogPostDetailSet(viewsets.ModelViewSet):
-    queryset = BlogPost.objects.all()
-    serializer_class = BlogpostDetailSerializer
-
-# Routers provide a way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'blog', BlogPostListSet)
 router.register(r'list', BlogPostDetailSet)
 
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))

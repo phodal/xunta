333d35c663e002bf031306a16050d499b2b92548
Phodal <h@phodal.com>
2015-04-17 12:47:22 +0000

    Add rest framework templates

--- settings.py
+++ settings.py
@@ -261,7 +261,6 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
     ],
     'PAGINATE_BY': 10,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'

89800af753fe897df10496f75f1f4f39e7ae1adf
Fengda HUANG <h@phodal.com>
2015-04-18 22:41:22 +0800

    Add loading images

--- static/mobile/js/views/BlogListView.js
+++ static/mobile/js/views/BlogListView.js
@@ -8,21 +8,27 @@ define([
 ],function($, Backbone, _, Mustache, blogListTemplate, BlogCollection){
 	'use strict';
 	var BlogListView = Backbone.View.extend ({
+		page: 0,
 		el: $("#content"),
 
 		initialize: function(){
 			var that = this;
 			this.collection = new BlogCollection();
 			this.collection.fetch({
-				data: {limit: 20},
+				data: { limit: 30 },
 				success: function(){
+					$(".loading").hide();
 					that.render();
+				}
+			});
+			$(window).scroll(function() {
+				if($(window).scrollTop() + $(window).height() == $(document).height()) {
+
 				}
 			});
 		},
 		render: function(){
 			var items= this.collection.toJSON()[0];
-			console.log(items.results);
 			this.$el.html(Mustache.to_html(blogListTemplate, { blog: items.results }));
 		}
 	});

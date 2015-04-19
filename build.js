c98c2d82011f07ee7198dbcf5b8f098630ebd839
Fengda HUANG <h@phodal.com>
2015-04-19 18:10:15 +0800

    mobile: test for requirejs build

---
+++ build.js
@@ -0,0 +1,25 @@
+({
+	baseUrl: 'static/mobile/',
+	paths: {
+		'text': 'js/lib/text',
+		jquery: 'js/lib/jquery.min',
+		json: 'js/lib/json',
+		appRouter: 'js/router',
+		templates: 'templates',
+		jquerySidr: 'js/lib/jquery.sidr.min',
+		touchwipe: 'js/lib/jquery.touchwipe.min',
+		underscore: 'js/lib/lodash.min',
+		mustache: 'js/lib/mustache',
+		backbone: 'js/lib/backbone'
+	},
+	shim: {
+		jquerySidr:["jquery"],
+		touchwipe: ["jquery"],
+		underscore: {
+			exports: '_'
+		}
+	},
+    name: "js/main",
+    out: "dist/build.js",
+    removeCombined: true
+});

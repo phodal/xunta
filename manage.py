54ca9bfe80d1f122e0a1196ea2550fe4b6dbb1f7
Phodal HUANG <h@phodal.com>
2017-02-26 21:34:01 +0800

    add deploy scripts

--- manage.py
+++ manage.py
@@ -3,20 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xunta.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
+
+    from mezzanine.utils.conf import real_project_name
+
+    settings_module = "%s.settings" % real_project_name("xunta")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+    from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
--- xunta/urls.py
+++ xunta/urls.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.views.decorators.cache import cache_page
+from django.views.i18n import set_language
 
 from homepage import views as home_view
 from comment import views as comment_views
@@ -11,15 +13,27 @@ from django.contrib.auth import views as auth_views
 from sitemaps.sitemaps import DisplayableSitemap
 from django.contrib.sitemaps import views as sitemap_views
 
+from xunta import settings
+
 admin.autodiscover()
 
 sitemaps = {"sitemaps": {"all": DisplayableSitemap}}
+
+urlpatterns = i18n_patterns(
+    # Change the admin prefix here to use an alternate URL for the
+    # admin interface, which would be marginally more secure.
+    url("^admin/", include(admin.site.urls)),
+)
+
+if settings.USE_MODELTRANSLATION:
+    urlpatterns += [
+        url('^i18n/$', set_language, name='set_language'),
+    ]
 
 urlpatterns = [
     url(r'^$', cache_page(60 * 60 * 6)(home_view.homepage)),
     url(r'^comment/$', comment_views.comment),
     url(r"^sitemap\.xml$", cache_page(60 * 60 * 6)(sitemap_views.sitemap), sitemaps),
-    url(r"^admin/", include(admin.site.urls)),
 
     url("^", include("links.urls")),
     url("^", include("juba.urls")),
@@ -33,4 +47,7 @@ urlpatterns = [
     url(r"^", include("mezzanine.urls")),
 ]
 
+# Adds ``STATIC_URL`` to the context of error pages, so that error
+# pages can use JS, CSS and images.
+handler404 = "mezzanine.core.views.page_not_found"
 handler500 = "mezzanine.core.views.server_error"

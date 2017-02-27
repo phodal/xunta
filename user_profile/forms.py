2621e77f757b9491fb8867c064491186fe46b883
Fengda HUANG <h@phodal.com>
2017-02-27 23:26:31 +0800

    clean some words

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,35 +1,30 @@
 from __future__ import unicode_literals
 
+from django import forms
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.tokens import default_token_generator
-from django.db.models import Q
 from django.db.models.manager import Manager
-from django import forms
 from django.utils.http import int_to_base36
 from django.utils.translation import ugettext, ugettext_lazy as _
-
 from mezzanine.accounts import (get_profile_model, get_profile_user_fieldname,
                                 get_profile_for_user, ProfileNotConfigured)
 from mezzanine.conf import settings
 from mezzanine.core.forms import Html5Mixin
 from mezzanine.utils.urls import slugify, unique_slug
 
+from user_profile.models import Profile
 
 User = get_user_model()
 
 _exclude_fields = tuple(getattr(settings,
                                 "ACCOUNTS_PROFILE_FORM_EXCLUDE_FIELDS", ()))
 
-# If a profile model has been configured with the ``AUTH_PROFILE_MODULE``
-# setting, create a model form for it that will have its fields added to
-# ``ProfileForm``.
-try:
-    class ProfileFieldsForm(forms.ModelForm):
-        class Meta:
-            model = get_profile_model()
-            exclude = (get_profile_user_fieldname(),) + _exclude_fields
-except ProfileNotConfigured:
-    pass
+
+class ProfileFieldsForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = (get_profile_user_fieldname(),) + _exclude_fields
+
 
 
 if settings.ACCOUNTS_NO_USERNAME:
@@ -45,11 +40,6 @@ class ProfileForm(Html5Mixin, forms.ModelForm):
     If a Profile model is defined via ``AUTH_PROFILE_MODULE``, its
     fields are injected into the form.
     """
-
-    password1 = forms.CharField(label=_("密码"),
-                                widget=forms.PasswordInput(render_value=False))
-    password2 = forms.CharField(label=_("Password (again)"),
-                                widget=forms.PasswordInput(render_value=False))
 
     class Meta:
         model = User
@@ -69,33 +59,19 @@ class ProfileForm(Html5Mixin, forms.ModelForm):
             # Make user fields required.
             if field in user_fields:
                 self.fields[field].required = True
-            # Disable auto-complete for password fields.
-            # Password isn't required for profile update.
-            if field.startswith("password"):
-                self.fields[field].widget.attrs["autocomplete"] = "off"
-                self.fields[field].widget.attrs.pop("required", "")
-                if not self._signup:
-                    self.fields[field].required = False
-                    if field == "password1":
-                        self.fields[field].help_text = ugettext(
-                                               "Leave blank unless you want "
-                                               "to change your password")
 
         # Add any profile fields to the form.
-        try:
-            profile_fields_form = self.get_profile_fields_form()
-            profile_fields = profile_fields_form().fields
-            self.fields.update(profile_fields)
-            if not self._signup:
-                user_profile = get_profile_for_user(self.instance)
-                for field in profile_fields:
-                    value = getattr(user_profile, field)
-                    # Check for multiple initial values, i.e. a m2m field
-                    if isinstance(value, Manager):
-                        value = value.all()
-                    self.initial[field] = value
-        except ProfileNotConfigured:
-            pass
+        profile_fields_form = self.get_profile_fields_form()
+        profile_fields = profile_fields_form().fields
+        self.fields.update(profile_fields)
+        if not self._signup:
+            user_profile = get_profile_for_user(self.instance)
+            for field in profile_fields:
+                value = getattr(user_profile, field)
+                # Check for multiple initial values, i.e. a m2m field
+                if isinstance(value, Manager):
+                    value = value.all()
+                self.initial[field] = value
 
     def clean_username(self):
         """
@@ -115,26 +91,6 @@ class ProfileForm(Html5Mixin, forms.ModelForm):
             return username
         raise forms.ValidationError(
                             ugettext("This username is already registered"))
-
-    def clean_password2(self):
-        """
-        Ensure the password fields are equal, and match the minimum
-        length defined by ``ACCOUNTS_MIN_PASSWORD_LENGTH``.
-        """
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-
-        if password1:
-            errors = []
-            if password1 != password2:
-                errors.append(ugettext("Passwords do not match"))
-            if len(password1) < settings.ACCOUNTS_MIN_PASSWORD_LENGTH:
-                errors.append(
-                        ugettext("Password must be at least %s characters") %
-                        settings.ACCOUNTS_MIN_PASSWORD_LENGTH)
-            if errors:
-                self._errors["password1"] = self.error_class(errors)
-        return password2
 
     def clean_email(self):
         """
@@ -171,24 +127,11 @@ class ProfileForm(Html5Mixin, forms.ModelForm):
                     username = self.cleaned_data["email"].split("@")[0]
                 qs = User.objects.exclude(id=self.instance.id)
                 user.username = unique_slug(qs, "username", slugify(username))
-        password = self.cleaned_data.get("password1")
-        if password:
-            user.set_password(password)
-        elif self._signup:
-            try:
-                user.set_unusable_password()
-            except AttributeError:
-                # This could happen if using a custom user model that
-                # doesn't inherit from Django's AbstractBaseUser.
-                pass
         user.save()
 
-        try:
-            profile = get_profile_for_user(user)
-            profile_form = self.get_profile_fields_form()
-            profile_form(self.data, self.files, instance=profile).save()
-        except ProfileNotConfigured:
-            pass
+        profile = get_profile_for_user(user)
+        profile_form = self.get_profile_fields_form()
+        profile_form(self.data, self.files, instance=profile).save()
 
         if self._signup:
             if (settings.ACCOUNTS_VERIFICATION_REQUIRED or

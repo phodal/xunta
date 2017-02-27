48a98403465f70d1d5d4a6397eae3cc47d782303
Fengda HUANG <h@phodal.com>
2017-02-27 23:32:07 +0800

    try to optional date

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget, AdminSplitDateTime
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.db.models.manager import Manager
+from django.forms import DateField, DateTimeField
 from django.utils.http import int_to_base36
 from django.utils.translation import ugettext, ugettext_lazy as _
 from mezzanine.accounts import (get_profile_model, get_profile_user_fieldname,
@@ -26,7 +28,6 @@ class ProfileFieldsForm(forms.ModelForm):
         exclude = (get_profile_user_fieldname(),) + _exclude_fields
 
 
-
 if settings.ACCOUNTS_NO_USERNAME:
     _exclude_fields += ("username",)
     username_label = _("Email address")
@@ -40,6 +41,8 @@ class ProfileForm(Html5Mixin, forms.ModelForm):
     If a Profile model is defined via ``AUTH_PROFILE_MODULE``, its
     fields are injected into the form.
     """
+
+    birthday = DateField(widget=AdminDateWidget)
 
     class Meta:
         model = User

1c00af788e941d5b78d24626a492db4d0e340a1d
Fengda HUANG <h@phodal.com>
2017-02-28 21:34:07 +0800

    create show models

--- show/models.py
+++ show/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from imagekit.models import ProcessedImageField
+
+from user_profile.models import Profile
+
+
+class Show(models.Model):
+    user_profile = models.ForeignKey(Profile, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    image = ProcessedImageField(upload_to='show',
+                                format='JPEG',
+                                options={'quality': 100})
+    posted_on = models.DateTimeField(default=datetime.now)
+
+    def get_number_of_likes(self):
+        return self.like_set.count()
+
+    def get_number_of_comments(self):
+        return self.comment_set.count()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Show')
+    user = models.ForeignKey(User)
+    comment = models.CharField(max_length=100)
+    posted_on = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.comment
+
+
+class Like(models.Model):
+    post = models.ForeignKey('Show')
+    user = models.ForeignKey(User)
+
+    class Meta:
+        unique_together = ("post", "user")
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' ' + self.post.title

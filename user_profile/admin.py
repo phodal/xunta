5c7c79b322fa35ce5b705ecd0a5dac2d156f689d
Fengda HUANG <h@phodal.com>
2017-03-02 07:52:00 +0800

    thinking in inline profile image

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from user_profile.models import Profile
+from user_profile.models import Profile, ProfileImage
+
+
+class ProfileImageInline(admin.TabularInline):
+    model = ProfileImage
+    extra = 3
 
 
 class ProfileAdmin(ModelAdmin):
     list_display = ("id", "user", "job", "address", "birthday")
     list_display_links = ("id",)
+    inlines = [ProfileImageInline, ]
 
 
 admin.site.register(Profile, ProfileAdmin)

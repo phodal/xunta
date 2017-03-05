7d048c4b4b79f7ad4f1090c6e89de4c838c22622
Phodal HUANG <h@phodal.com>
2017-03-05 16:10:36 +0800

    add profile image for profile

--- api/profile.py
+++ api/profile.py
@@ -1,16 +1,26 @@
+from django.contrib.auth.models import User
 from rest_framework import filters
 from rest_framework import serializers, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django.contrib.auth.models import User
-from user_profile.models import Profile
+
+from user_profile.models import Profile, ProfileImage
+
+
+class ProfileImageSerializer(serializers.ModelSerializer):
+    image = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = ProfileImage
+        fields = ('image', 'comments')
 
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.SerializerMethodField('get_username_by_id')
+    images = ProfileImageSerializer(many=True, required=False, read_only=True)
 
     class Meta:
         model = Profile
-        fields = ('user', 'birthday', 'constellate', 'height', 'interest')
+        fields = ('user', 'birthday', 'constellate', 'height', 'interest', 'images')
 
     @staticmethod
     def get_username_by_id(model):

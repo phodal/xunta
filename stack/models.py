60c9605fa91b451dc549eb2ebf3d3ae92270dc46
Phodal HUANG <h@phodal.com>
2017-03-05 16:43:47 +0800

    [stacks] thinking in model

--- stack/models.py
+++ stack/models.py
@@ -1,11 +1,13 @@
 from django.urls import reverse
 from django.db import models
 from mezzanine.core.fields import RichTextField
-from mezzanine.core.models import Displayable
+from mezzanine.core.models import Displayable, TimeStamped, MetaData
 from mezzanine.generic.fields import RatingField
 from django.utils.translation import ugettext_lazy as _
 
 from xunta import settings
+
+USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 class Stack(Displayable):
@@ -19,3 +21,44 @@ class Stack(Displayable):
 
     def get_absolute_url(self):
         return reverse("stack_detail", kwargs={"slug": self.slug})
+
+
+class Job(MetaData, TimeStamped):
+    class Meta:
+        verbose_name = _('工作')
+        verbose_name_plural = _('工作')
+
+    name = models.CharField()
+    content = RichTextField(null=True, blank=(not getattr(settings, "Company_REQUIRED", False)))
+
+
+class GitHubInfo(models.Model):
+    name = models.CharField()
+    slug = models.SlugField()
+    content = RichTextField()
+
+
+class Company(Displayable):
+    class Meta:
+        verbose_name = _('公司')
+        verbose_name_plural = _('公司')
+
+    content = RichTextField(null=True, blank=(not getattr(settings, "Company_REQUIRED", False)))
+    stacks = models.ForeignKey(Stack)
+    jobs = models.ForeignKey(Job)
+
+    def get_absolute_url(self):
+        return reverse("company_detail", kwargs={"slug": self.slug})
+
+
+class Programmer(models.Model):
+    class Meta:
+        verbose_name = _('程序员')
+        verbose_name_plural = _('程序员')
+
+    user = models.OneToOneField(USER_MODEL)
+    current_stack = models.ForeignKey(Stack)
+    future_stack = models.ForeignKey(Stack)
+    contact = models.CharField()
+    company = models.CharField(Company)
+    blog = models.SlugField()

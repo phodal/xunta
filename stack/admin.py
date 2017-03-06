c89b5bdff969b41931b0554a61dd1803aade1db5
Fengda HUANG <h@phodal.com>
2017-03-06 23:10:32 +0800

    hidden description

--- stack/admin.py
+++ stack/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from stack.models import Stack, Company, Programmer, GitHubInfo, Job
 
 
-admin.site.register(Stack)
+class StackAdmin(ModelAdmin):
+    list_display = ("id", "title", "slug", "created")
+    list_display_links = ("id",)
+    exclude = ('description',)
+
+admin.site.register(Stack, StackAdmin)
 admin.site.register(Company)
 admin.site.register(Programmer)
 admin.site.register(GitHubInfo)

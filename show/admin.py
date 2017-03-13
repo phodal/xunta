58aa3b34b0a9540c1f284765599527625ca5cfce
Fengda HUANG <h@phodal.com>
2017-03-13 22:42:12 +0800

    update show admin

--- show/admin.py
+++ show/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from mezzanine.core.admin import OwnableAdmin
 
 from show.models import Show, Like, Comment
 
+class ShowAmin(OwnableAdmin):
+
+    list_display = ("id", "title", "user", "posted_on")
+    ordering = ("-posted_on",)
+
+    fieldsets = (
+        (None, {
+            "fields": ("title", "image"),
+        }),
+    )
+
 admin.site.register(Comment)
 admin.site.register(Like)
-admin.site.register(Show)
+admin.site.register(Show, ShowAmin)

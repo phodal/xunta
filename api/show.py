d6705970232fd0bffc7a329a245daa8d7d043671
Phodal HUANG <h@phodal.com>
2017-03-14 12:58:06 +0800

    [T] show api: add different serieali

--- api/show.py
+++ api/show.py
@@ -2,10 +2,20 @@ from rest_framework import filters
 from rest_framework import serializers, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from api.common_serializer import UserSerializer
+
 from show.models import Show
 
 
-class ShowSerializer(serializers.HyperlinkedModelSerializer):
+class ShowGetSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer(required=True)
+
+    class Meta:
+        model = Show
+        fields = ('title', 'user', 'image', 'posted_on')
+
+
+class ShowPostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Show
         fields = ('title', 'image', 'posted_on')
@@ -13,10 +23,14 @@ class ShowSerializer(serializers.HyperlinkedModelSerializer):
 
 class ShowSet(viewsets.ModelViewSet):
     queryset = Show.objects.filter()
-    serializer_class = ShowSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('title', )
+    search_fields = ('title',)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return ShowPostSerializer
+        return ShowGetSerializer
